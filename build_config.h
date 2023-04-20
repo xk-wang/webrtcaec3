28e2600d01bfd48624f7b35c801cce6e5a1ee5e1
xk_wang <xk_wang@qq.com>
2023-04-20 18:07:33 +0800

    initial

---
+++ build_config.h
@@ -0,0 +1,19 @@
+#ifndef AEC3_BUILD_CONFIG
+#define AEC3_BUILD_CONFIG
+
+/************ checks ************/
+// #define DCHECK_ALWAYS_ON
+#define RTC_DISABLE_CHECK_MSG
+
+/*********** aec dump ***********/
+#define WEBRTC_APM_DEBUG_DUMP 0
+
+// #define RTC_CHECK_MSG_ENABLED 1
+
+/********* field trial **********/
+//#define WEBRTC_EXCLUDE_FIELD_TRIAL_DEFAULT
+
+/*********** metrics ************/
+//#define RTC_DISABLE_METRICS
+
+#endif //AEC3_BUILD_CONFIG
